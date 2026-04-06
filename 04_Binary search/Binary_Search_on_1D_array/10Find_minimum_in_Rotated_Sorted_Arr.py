77873b2af27ce37b00ff14e26a612dec6a8dddb9
kedarsutar-git <kedarsutar37@gmail.com>
2026-04-06 22:20:33 +0530

    updated binary search folder

--- 04_Binary search/Binary_Search_on_1D_array/10Find_minimum_in_Rotated_Sorted_Arr.py
+++ 04_Binary search/Binary_Search_on_1D_array/10Find_minimum_in_Rotated_Sorted_Arr.py
@@ -17,3 +17,46 @@ nums = [11,13,15,17]
 Output: 11
 Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
 '''
+
+# Brute Force method
+class Solution:
+    def find_min(self,nums:list[int]) ->int:
+        MIN = nums[0]
+        for i in range(len(nums)):
+           
+            if(nums[i]<MIN):
+                MIN = min(MIN,nums[i])
+
+        return MIN
+nums = [11,13,15,17]   
+object = Solution()
+print(object.find_min(nums))         
+'''
+Time Complixity:O(n)
+Space Complixity:O(1)
+'''
+
+
+# Optimal method
+class Solution:  
+    def find_min(self, nums: list[int]) -> int:
+        start = 0
+        end = len(nums) - 1
+
+        while start < end:
+            mid = start + (end - start) // 2
+
+            if nums[mid] > nums[end]:
+                # Minimum is in the RIGHT half
+                start = mid + 1
+            else:
+                # Minimum is in the LEFT half (mid could be the answer)
+                end = mid
+ 
+        return nums[start]
+
+nums = [4, 5, 6, 7, 1, 2]
+obj = Solution()
+print(obj.find_min(nums))  # Output: 1
+
+
---
+++ 04_Binary search/Binary_Search_on_1D_array/12_Single_element_in_sorted_arr.py
@@ -0,0 +1,63 @@
+'''
+Example 1:
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+
+
+Example 2:
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
+'''
+
+# Brute force method
+
+class Solution:
+    def Single_element(Self,nums:list[int]) -> int:
+        for i in range(1,len(nums)-1):
+            if(nums[i-1]!=nums[i]!=nums[i+1]):
+                return nums[i]
+            
+nums = [3,3,7,7,10,11,11]   
+object = Solution()
+print(object.Single_element(nums))        
+
+'''
+Time Complixity:O(n)
+Space Complixity:O(1)
+
+'''
+
+# Optimal method
+
+class Solution:
+    def Find_single_element(self,nums:list[int]) ->int:
+       
+        if(len(nums)==1):
+            return nums[0]
+        
+        if(nums[0]!=nums[1]):
+            return nums[0]
+        
+        if(nums[len(nums)-1]!=nums[len(nums)-2]):
+            return nums[len(nums)-1]
+        
+        start = 1
+        end = len(nums) - 2
+        while(start<=end):
+            mid = start+(end-start)//2
+            if(nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]):
+                return nums[mid]
+            
+            if(mid%2==1 and nums[mid-1]==nums[mid] or mid%2==0 and nums[mid+1]==nums[mid]):
+                start = mid+1
+
+            else:
+                end = mid-1
+
+        return -1
+
+nums = [3,3,7,7,10,11,11]   
+object = Solution()
+print(object.Find_single_element(nums))          
+
+            
