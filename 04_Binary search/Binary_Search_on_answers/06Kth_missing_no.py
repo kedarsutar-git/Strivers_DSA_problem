63f38b1cdf70a7bbc81ad0a0cb39f7d3ddb25cff
kedarsutar-git <kedarsutar37@gmail.com>
2026-04-11 23:26:26 +0530

    updated Binary search folder

--- 04_Binary search/Binary_Search_on_answers/06Kth_missing_no.py
+++ 04_Binary search/Binary_Search_on_answers/06Kth_missing_no.py
@@ -19,5 +19,44 @@ Time Complixity:O(n**2)
 Space Complixity:O(n)
 
 '''
+# better Approach
+class Solution:
+    def kth_miss(self,nums:list[int],k:int) ->int:
+        for i in range(len(nums)):
+            if(nums[i]<=k):
+                k+=1
 
+            else:
+                break
+        return k
+nums = [2,3,4,7,11]
+object =Solution()  
+print(object.kth_miss(nums,5))
+'''
+Time Complixity:O(n)
+Space Complixity:O(1)   
+'''
 
+# optimized Approach
+              
+
+class Soution:
+    def kth_miss(self,nums:list[int],k:int) ->int:
+        start = 0
+        end = len(nums)-1   
+        while start<=end:
+            mid = start + (end-start)//2
+            miss = nums[mid] - (mid+1)
+            if miss<k:
+                start = mid+1
+            else:
+                end = mid-1
+        return k + end + 1
+nums = [2,3,4,7,11]
+object =Soution()       
+print(object.kth_miss(nums,5))
+'''
+Time Complixity::O(logn)
+space Complixity:O(1)
+
+'''    
