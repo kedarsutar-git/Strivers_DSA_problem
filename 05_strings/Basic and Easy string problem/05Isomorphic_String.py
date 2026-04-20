4cd6c54c7abe9337b3f15b836234917d099fc00c
kedarsutar-git <kedarsutar37@gmail.com>
2026-04-20 23:31:10 +0530

    updated string folder

---
+++ 05_strings/Basic and Easy string problem/05Isomorphic_String.py
@@ -0,0 +1,39 @@
+'''
+Example 1
+Input:
+s = "paper", t = "title"
+Output:
+true
+Explanation:
+ The characters in "s" can be mapped one-to-one to characters in "t": 
+'p' → 't', 'a' → 'i', 'e' → 'l', 'r' → 'e'
+Since the mapping is consistent and unique for each character, the strings are isomorphic.
+
+
+Example 2
+Input:
+s = "foo", t = "bar"
+Output:
+false
+Explanation:
+'f' → 'b' is fine, 'o' → 'a' for the first 'o', But the second 'o' in "s" would need to map to 'r' in "t", which conflicts with the earlier mapping of 'o' → 'a'
+This inconsistency makes it impossible to convert "s" to "t" using a one-to-one character mapping.
+'''
+
+class Solution:
+    def IsomorphicString(self,s:str,t:str) -> bool:
+        m1 = [0]*256
+        m2 = [0]*256
+        for i in range(len(s)):
+            if(m1[ord(s[i])]!=m2[ord(t[i])]):
+                return False
+            
+            m1[ord(s[i])] =i+1
+            m2[ord(t[i])] =i+1
+
+        return True
+        
+s = "paper" 
+t = "title" 
+object = Solution()
+print(object.IsomorphicString(s,t))       
---
+++ 05_strings/Basic and Easy string problem/06Check_String_rotation.py
@@ -0,0 +1,42 @@
+'''
+Example 1:
+Input:
+s = "rotation", goal = "tionrota"
+Output:
+ true
+Explanation:
+After multiple left shifts on "rotation", we get:
+    1st shift → "otationr"
+    2nd shift → "tationro"
+    3rd shift → "ationrot"
+    4th shift → "tionrota"
+    So the goal string can be obtained by rotating the original string.
+
+Example 2:
+Input:
+s = "hello", goal = "lohelx"
+Output:
+false
+Explanation:
+ 
+Even after all possible rotations of "hello", we cannot form "lohelx" due to the presence of an extra character 'x'. Hence, it's not possible.
+         
+'''
+class Solution:
+    def CheckString(self,s:str,goal:str):
+        result = s+s
+        
+        if(len(s)!=len(goal)):
+            return False
+        
+        return goal in result
+             
+s = "rotation"
+goal = "tionrota"
+object =Solution()
+print(object.CheckString(s,goal))
+'''
+Time Comlexity:O(1)
+Space Complexity:O(1)
+
+'''
