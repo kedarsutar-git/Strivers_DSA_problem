6f5209341ab3f3b2d0a6808308d7430ee733e574
kedarsutar-git <kedarsutar37@gmail.com>
2026-06-27 19:11:25 +0530

    updated learn Bits Manipulation folder

---
+++ 08_Bits Manipulation/01Learn Bits Manipulation/05Set-the-ith-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def Set_ith_bits(self,n:int,i:int) ->int:
+        return (n|(1<<i))
+    
+object = Solution()
+print(object.Set_ith_bits(9,2))
+'''
+Time Complexity:O(1)
+Space Complexity:O(1)
+
+'''
