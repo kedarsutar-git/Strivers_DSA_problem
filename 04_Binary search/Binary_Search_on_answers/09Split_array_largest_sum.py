0504cc7030df090c576dd65d0d5d13b8911c65d0
kedarsutar-git <kedarsutar37@gmail.com>
2026-07-02 22:58:51 +0530

    updated Binary Search on Answer folder

--- 04_Binary search/Binary_Search_on_answers/09Split_array_largest_sum.py
+++ 04_Binary search/Binary_Search_on_answers/09Split_array_largest_sum.py
@@ -21,37 +21,48 @@ sum among the two subarrays is only 9.
 '''
 
 class Solution:
-    def splitArray(self, nums: list[int], k: int) -> int:
+    def SplitArray(self,nums:list[int],k:int) ->int:
         if(k>len(nums)):
-            return -1
-
+            return -1 
+        
         start = max(nums)
         end = sum(nums)
+        ans = -1
 
         while(start<=end):
-            mid = start +(end-start)//2
-            Sum = 0
-            split = 1
-            for num in nums:
-                if(Sum+num<=mid):
-                    Sum +=num
-                else:
-                    split +=1
-                    Sum = num
-                    
-            if(split>k):
-                start = mid+1
+            mid = start + (end - start)//2
+
+            if self.isValid(nums,mid,k):
+                ans = mid
+                end = mid - 1
 
             else:
-                end = mid-1
+                start = mid + 1
 
-        return start                            
-        
+        return ans 
+
+    def isValid(self,nums,mid,k):
+        split = 1
+        Sum = 0
+
+        for num in nums:
+            if(Sum+num<=mid):
+                Sum +=num
+
+            else:
+                split +=1
+                Sum = num
+
+        if(split>k):
+            return False
+        return True 
 nums =  [10, 20, 30, 40]
 object = Solution()
-print(object.splitArray(nums, 2)) 
+print(object.SplitArray(nums,2))    
 '''
 Time Complexity:O(nlogn)
 Space Complexity:O(1)
 
 '''
+
+        
