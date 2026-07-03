46e69e4f3540e6b6ab4be569814a4e1e69c9c9d0
kedarsutar-git <kedarsutar37@gmail.com>
2026-07-03 18:21:46 +0530

    Add Slinding Window and Two Pointer folder

---
+++ 09_Sliding-window-and-Two-pointer/01Basic-Sliding-Window-problem.py
@@ -0,0 +1,20 @@
+class Solution:
+    def SubarraySum(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+
+        # Sum of first window
+        currentSum = sum(nums[:k])
+        Max = currentSum
+
+        # Slide the window
+        for i in range(k, n):
+            currentSum = currentSum + nums[i] - nums[i-k]
+            Max = max(Max, currentSum)
+
+        return Max
+
+
+obj = Solution()
+nums = [2, 3, 5, 2, 9, 7, 1]
+print(obj.SubarraySum(nums, 3))
+            
