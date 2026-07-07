97307ef0ee92a50d800b50ba99519f87fa0725ac
kedarsutar-git <kedarsutar37@gmail.com>
2026-07-07 20:51:04 +0530

    updated Bits Manipulation folder

--- 08_Bits Manipulation/01Learn Bits Manipulation/15SingleNumber-III.py
+++ 08_Bits Manipulation/01Learn Bits Manipulation/15SingleNumber-III.py
@@ -19,12 +19,28 @@ The integers -1 and 0 have appeared only once.
 '''
 class Solution:
     def SingleNumber(self,nums:list[int]) ->list[int]:
-        ans = 0
-        for i in range(len(nums)):
-            ans = ans ^nums[i]
+        Map = {}
+        for num in nums:
+            if num in Map:
+                Map[num] +=1
 
-            
-        return ans 
+            else:
+                Map[num] = 1
+
+        ans =[] 
+
+        for key,value in Map.items():
+            if(value==1):
+                ans.append(key)
+
+        return ans       
+
 object = Solution()
 nums = [1, 2, 1, 3, 5, 2]
-print(object.SingleNumber(nums))            
+print(object.SingleNumber(nums)) 
+
+'''
+Time Complexity:O(n)
+Space Complexity:O(n)
+
+'''
