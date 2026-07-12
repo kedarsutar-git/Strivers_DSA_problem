9b73d8d0e4384451f6b57f22864d4660bc912d6b
kedarsutar-git <kedarsutar37@gmail.com>
2026-07-12 22:26:34 +0530

    updated Sliding window and two pointer folder

--- 09_Sliding-window-and-Two-pointer/01Basic Sliding window problem/07Binary-subarr-with-sum-equal-to-k.py
+++ 09_Sliding-window-and-Two-pointer/01Basic Sliding window problem/07Binary-subarr-with-sum-equal-to-k.py
@@ -1,6 +1,6 @@
 '''
 Input: nums = [1, 0, 0, 1, 1, 0], goal = 2  
-Output: 6
+Output: 7
 Explanation: There are 6 subarrays with sum exactly equal to 2:
 [1, 0, 0, 1], [0, 0, 1, 1], [0, 1, 1], [1, 1], [1, 1, 0], [0,0,1,1,0]
 
@@ -33,3 +33,37 @@ Time Complexity:O(n**2)
 Space Complexity:O(1)
 
 '''
+
+# Optimal method 
+
+class Solution:
+
+    def AtMost(self, nums: list[int], goal: int) -> int:
+        if goal < 0:
+            return 0
+
+        left = 0
+        right = 0
+        Sum = 0
+        count = 0
+
+        while right < len(nums):
+            Sum += nums[right]
+
+            while Sum > goal:
+                Sum -= nums[left]
+                left += 1
+
+            count += (right - left + 1)
+            right += 1
+
+        return count
+
+    def BinarySubarrSum(self, nums: list[int], goal: int) -> int:
+        return self.AtMost(nums, goal) - self.AtMost(nums, goal - 1)
+
+
+obj = Solution()
+
+nums = [1, 0, 0, 1, 1, 0]
+print(obj.BinarySubarrSum(nums, 2))
