bff6058a9287557ba4e8186a46697b00d8d32cbd
kedarsutar-git <kedarsutar37@gmail.com>
2026-07-16 11:30:08 +0530

    updated Sliding window and two pointer folder

--- 09_Sliding-window-and-Two-pointer/02Hard Problem/03Minimum-Window-Substring.py
+++ 09_Sliding-window-and-Two-pointer/02Hard Problem/03Minimum-Window-Substring.py
@@ -137,3 +137,51 @@ print(obj.minWindow(s, t))
 
 # Optimal method 
 
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        n = len(s)
+        m = len(t)
+        count_map = {}
+        for char in t:
+            if char in count_map:
+                count_map[char] +=1
+
+            else:
+                count_map[char] = 1
+
+        left,right = 0, 0
+        count = 0
+        startindex = -1
+        minlen = float("inf")
+
+        while(right<n):
+            if(count_map.get(s[right], 0)>0):
+                count += 1
+
+            count_map[s[right]] = count_map.get(s[right],0) - 1
+
+            while(count==m):
+                if(right-left+1<minlen):
+                    minlen = right-left+1
+                    startindex = left
+
+                count_map[s[left]]  = count_map.get(s[left],0) + 1
+
+                if(count_map[s[left]]>0):
+                    count -=1
+
+                left +=1
+            right +=1
+
+        if startindex == -1:
+            return ""
+
+        return s[startindex:startindex + minlen]
+
+s = "ADOBECODEBANC"
+t = "ABC"                     
+object = Solution()
+print(object.minWindow(s,t))
+
+
+        
