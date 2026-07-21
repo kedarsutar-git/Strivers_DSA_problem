40f2fad11cf6d7599639bec80c087075f6b610f4
kedarsutar-git <kedarsutar37@gmail.com>
2026-07-21 20:56:51 +0530

    updated Recursion folder

--- 07_Recursion/01Get a strong Hold/14Countdigits.py
+++ 07_Recursion/01Get a strong Hold/14Countdigits.py
@@ -1,13 +1,13 @@
 class Solution:
-    def printdig(self,n:int):
+    def countdigits(self,n:int):
         if(n==0):
             return 
         
         digit = n%10
         n = n//10
 
-        self.printdig(n)
+        self.countdigits(n)
         print(digit,end=" ")
 
 object = Solution()
-object.printdig(1234)
+object.countdigits(1234)
