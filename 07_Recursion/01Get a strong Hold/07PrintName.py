16249be99d4efc33a6bc8d86723a5674399b1911
kedarsutar-git <kedarsutar37@gmail.com>
2026-07-21 20:59:45 +0530

    updated Recursion folder

--- 07_Recursion/01Get a strong Hold/07PrintName.py
+++ 07_Recursion/01Get a strong Hold/07PrintName.py
@@ -11,15 +11,4 @@ object = Solution()
 print(object.printName(10))    
 
 
-class Solution:
-    def printnumber(self,n:int,count):
-        if(n<count):
-            return 
         
-        self.printnumber(n,count+1)
-        print(count)
-        
-
-
-object = Solution()
-print(object.printnumber(10,1))        
