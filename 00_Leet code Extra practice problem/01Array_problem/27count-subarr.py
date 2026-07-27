4bb0543f485016c515cc24a03eaafa7ffbef8e83
kedarsutar-git <kedarsutar37@gmail.com>
2026-07-27 21:50:52 +0530

    updated Array folder

---
+++ 00_Leet code Extra practice problem/01Array_problem/27count-subarr.py
@@ -0,0 +1,58 @@
+'''
+You are given an integer array nums and a positive integer k.
+
+Return the number of subarrays where the maximum element of nums appears at least k times in that subarray.
+
+A subarray is a contiguous sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,2,3,3], k = 2
+Output: 6
+Explanation: The subarrays that contain the element 3 at least 2 times are: [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+Example 2:
+
+Input: nums = [1,4,2,1], k = 3
+Output: 0
+Explanation: No subarray contains the element 4 at least 3 times.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+1 <= k <= 105
+'''
+
+# Brute force method 
+class Solution:
+    def countSubarr(self,nums:list[int],k:int) ->int:
+        maxnum = max(nums)
+        count = 0
+
+        for i in range(len(nums)):
+            count_map = {}
+            for j in range(i,len(nums)):
+
+                if nums[j] in count_map:
+                    count_map[nums[j]] +=1
+
+                else:
+                    count_map[nums[j]] = 1
+
+                if(count_map.get(maxnum,0)>=k):
+                    count +=1
+
+        return count
+
+object = Solution()
+nums = [1,3,2,3,3]
+print(object.countSubarr(nums,2))
+
+'''
+Time Complexity:O(n^2)
+Sapce Complexity:O(n)
+
+'''
