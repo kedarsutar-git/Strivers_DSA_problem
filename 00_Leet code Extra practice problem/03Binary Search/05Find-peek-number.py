93805ca06407ad88e6f821025f808f1e62b1e74a
kedarsutar-git <kedarsutar37@gmail.com>
2026-08-14 20:56:38 +0530

    updated Binary Search folder

--- 00_Leet code Extra practice problem/03Binary Search/05Find-peek-number.py
+++ 00_Leet code Extra practice problem/03Binary Search/05Find-peek-number.py
@@ -60,4 +60,33 @@ Space Complexity:O(1)
 
 # optimal method 
 
+class Solution:
+    def peaknumberindex(self,nums:list[int]) ->int:
+        start = 1
+        end = len(nums)-2
 
+        while(start<=end):
+            mid = start + (end - start)//2
+
+            if(nums[mid-1]<nums[mid] and nums[mid+1]<nums[mid]):
+                return mid
+
+            elif(nums[mid-1]<nums[mid]):
+                start = mid + 1
+
+            else:
+                end = mid - 1
+
+        return -1
+
+nums =  [0,2,1,0]
+
+object = Solution()
+print(object.peaknumberindex(nums))
+
+
+'''
+Time Complexity:O(logn)
+space complexity:O(1)
+
+'''
