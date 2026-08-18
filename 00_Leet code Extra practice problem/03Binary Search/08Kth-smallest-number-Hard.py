d7ae1833e3360dac20587c3fe24a5951e666bd2b
kedarsutar-git <kedarsutar37@gmail.com>
2026-08-19 00:43:04 +0530

    updated Binary search folder

---
+++ 00_Leet code Extra practice problem/03Binary Search/08Kth-smallest-number-Hard.py
@@ -0,0 +1,87 @@
+'''
+Nearly everyone has used the Multiplication Table. The multiplication table of size m x n is an integer matrix mat where mat[i][j] == i * j (1-indexed).
+
+Given three integers m, n, and k, return the kth smallest element in the m x n multiplication table.
+
+ 
+
+Example 1:
+
+
+Input: m = 3, n = 3, k = 5
+
+Matrix:
+
+1  2  3
+2  4  6
+3  6  9
+
+Sorted elements:
+
+1  2  2  3  3  4  6  6  9
+
+Input:
+m = 3, n = 3, k = 5
+
+Output: 3
+Explanation: The 5th smallest number is 3.
+
+
+
+Example 2:
+
+Matrix:
+
+1  2  3
+2  4  6
+
+Sorted elements:
+
+1  2  2  3  4  6
+
+Input:
+m = 2, n = 3, k = 6
+Input: m = 2, n = 3, k = 6
+Output: 6
+Explanation: The 6th smallest number is 6.
+ 
+
+Constraints:
+
+1 <= m, n <= 3 * 104
+1 <= k <= m * n
+'''
+
+class Solution:
+    def KthSmallestnumber(self,m:int,n:int,k:int) ->int:
+        matrix = []
+        for i in range(1,n+1):
+            row = []
+            for j in range(1,m+1):
+                row.append(i*j)
+
+            matrix.append(row)
+
+
+        arr =[]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                arr.append(matrix[i][j])
+
+        arr.sort()
+
+        count = 0
+        for x in range(len(arr)):
+            count += 1
+            if(count==k):
+                ans = arr[x]
+
+        return ans
+
+object = Solution()
+print(object.KthSmallestnumber(3,3,5))
+
+'''
+Time Complexity : O(nm log(nm))	
+Space Complexity: O(nm)
+'''
